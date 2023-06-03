15ebe973322410308de44ebd30f9683f36adea11
Amy Smith <smithniamyfse@gmail.com>
2023-06-03 01:37:47 -0500

    Added 6 albums arguments to the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,7 +5,6 @@ let collection = [];
 
 // TODO - add function addToCollection with 3 parameters
 function addToCollection(title, artist, yearPublished) {
-    console.log('in addToCollection:', title, artist, year);
     // TODO - create new object using 3 parameters as properties
     const album = {
         title: title,
@@ -18,3 +17,15 @@ function addToCollection(title, artist, yearPublished) {
     return album;
 } // end addToCollection
 
+// TODO - test the addToCollection function with 6 various albums
+addToCollection('Apologies to Queen Mary', 'Wolf Parade', 2005);
+
+addToCollection('In Evening Air', 'Future Islands', 2010);
+
+addToCollection('The Immaculate Collection', 'Madonna', 1990);
+
+addToCollection('Astro Lounge', 'Smash Mouth', 1999);
+
+addToCollection('Torches', 'Foster the People', 2011);
+
+addToCollection('Easter', 'Patti Smith', 1978);
