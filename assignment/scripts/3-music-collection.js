baf7f83cdca91e1cdf34e57e54d59ee9d2d3cee2
Amy Smith <smithniamyfse@gmail.com>
2023-06-03 00:23:04 -0500

    Added and updated // comments; created object with 3 properties

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,18 @@
 console.log('***** Music Collection *****')
 
+// TODO - create variable collection assign empty array
 let collection = [];
 
+// TODO - add function addToCollection with 3 parameters
 function addToCollection(title, artist, yearPublished) {
-    
-}
+    console.log('in addToCollection:', title, artist, year);
+    // TODO - create new object using 3 parameters as properties
+    const addToCollection = {
+        title: 
+        age:
+        year: 
+    } // end object addToCollection
+
+} // end variable addToCollection
+
+// TODO - Create 
