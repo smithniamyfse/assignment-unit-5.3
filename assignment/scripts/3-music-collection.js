2201ba3bfaf131883855271b2ab2edb985da1f0f
Amy Smith <smithniamyfse@gmail.com>
2023-06-04 18:08:03 -0500

    function showCollection: logged number of albums in the collection; for loop and log album's information; tested showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,18 +18,28 @@ function addToCollection(title, artist, yearPublished) {
 } // end addToCollection
 
 // Test the addToCollection function with 6 various albums and log returned value
-console.log(addToCollection('Apologies to Queen Mary', 'Wolf Parade', 2005));
-console.log(addToCollection('Apologies to Queen Mary', 'Wolf Parade', 2005));
-console.log(addToCollection('In Evening Air', 'Future Islands', 2010));
-console.log(addToCollection('The Immaculate Collection', 'Madonna', 1990));
-console.log(addToCollection('Astro Lounge', 'Smash Mouth', 1999));
-console.log(addToCollection('Torches', 'Foster the People', 2011));
-console.log(addToCollection('Easter', 'Patti Smith', 1978));
+let albumOne = addToCollection('Apologies to Queen Mary', 'Wolf Parade', 2005);
+console.log(albumOne);
+let albumTwo = addToCollection('In Evening Air', 'Future Islands', 2010);
+console.log(albumTwo);
+let albumThree = addToCollection('The Immaculate Collection', 'Madonna', 1990);
+console.log(albumThree);
+let albumFour = addToCollection('Astro Lounge', 'Smash Mouth', 1999);
+console.log(albumFour);
+let albumFive = addToCollection('Torches', 'Foster the People', 2011);
+console.log(albumFive);
+let albumSix = addToCollection('Easter', 'Patti Smith', 1978);
+console.log(albumSix);
 
 // Log the collection array
 console.log(collection);
 
-// Add function showCollection 
-function showCollection () {
-
-} // end showCollection
+// Add function showCollection with collection array as its parameter
+function showCollection(collection) {
+    // Log the number of albums in the collection
+    console.log('Number of albums in the collection:', collection.length);
+    for (let i = 0; i < collection.length; i++) {
+      console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
+    } // end loop over array
+  }
+  showCollection(collection);
