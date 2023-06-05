04967f446dbba26bf53a50a3f1378f3a7eda33db
Amy Smith <smithniamyfse@gmail.com>
2023-06-05 01:07:58 -0500

    function findByArtist: Added ability to pass string variable; created empty array variable artistResults; created for loop with if conditional statement to find artist property matches and add to artistResults array; return matching results or if no results, return empty array; tested 3 different ways with different artist arguments

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,12 +10,12 @@ function addToCollection(title, artist, yearPublished) {
         title,
         artist,
         yearPublished,
-    }; // end album
+    }; // end object album
     // Add object album to end of collection array
     collection.push(album);
     // Return object album
     return album;
-} // end addToCollection
+} // end function addToCollection
 
 // Test the addToCollection function with 6 various albums and log returned value
 let albumOne = addToCollection('Apologies to Queen Mary', 'Wolf Parade', 2005);
@@ -28,8 +28,10 @@ let albumFour = addToCollection('Astro Lounge', 'Smash Mouth', 1999);
 console.log(albumFour);
 let albumFive = addToCollection('Torches', 'Foster the People', 2011);
 console.log(albumFive);
-let albumSix = addToCollection('Easter', 'Patti Smith', 1978);
+let albumSix = addToCollection('Sgt. Peppers Lonely Hearts Club Band', 'The Beatles', 1967);
 console.log(albumSix);
+let albumSeven = addToCollection('Let It Be', 'The Beatles', 1970);
+console.log(albumSeven);
 
 // Log the collection array
 console.log(collection);
@@ -40,12 +42,36 @@ function showCollection(collectionName) {
     console.log('Number of albums in the collection:', collectionName.length);
     for (let i = 0; i < collectionName.length; i++) {
         console.log(`${collectionName[i].title} by ${collectionName[i].artist}, published in ${collectionName[i].yearPublished}`);
-    } // end loop over array
-}
+    } // end for loop
+} // end function showCollection 
 
 // Test 1: Testing showCollection by sending collection array variable
-showCollection(collection); // end Test 1
+showCollection(collection); 
+
 // Test 2: Testing showCollection by adding new album to collection
-let albumSeven = addToCollection('Sgt. Peppers Lonely Hearts Club Band', 'The Beatles', 1967);
-console.log(albumSeven);
-showCollection(collection); // end Test 2
+let albumEight = addToCollection('Easter', 'Patti Smith', 1978);
+console.log(albumEight);
+showCollection(collection);
+
+// Add function findByArtist pass string variable 
+function findByArtist(artistName) {
+    // Create empty array variable
+    let artistResults = [];
+    // Loop through collection to find artist and add property matches to artistResults array
+    for (let i = 0; i < collection.length; i++) {
+        if (artistName === collection[i].artist) {
+            artistResults.push(collection[i]);
+        }
+    }
+    // Return array with matching results or an enmpty array if none found
+    return artistResults;
+} // end function artistName 
+
+// Test 1: Testing findByArtist with known artist as arguments 
+console.log(findByArtist('Wolf Parade'));
+
+// Test 2: Testing findByArtist with 2 collections from same artist
+console.log(findByArtist('The Beatles'));
+
+// Test 3: Testing findByArtist with value not in collection
+console.log(findByArtist('The B-52s'));
